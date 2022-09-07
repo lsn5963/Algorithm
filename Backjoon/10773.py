6fd1b2a588ed44202ea71aa299c0926e916dd7b0
이상남 <lsn5963@naver.com>
2022-09-07 23:29:27 +0900

    feat: 제로 10773

---
+++ Backjoon/10773.py
@@ -0,0 +1,19 @@
+import sys
+
+k = int(sys.stdin.readline().rstrip())
+
+data = []
+
+for _ in range(k):
+    t = int(sys.stdin.readline().rstrip())
+    if t == 0:
+        data.pop()
+    else:
+        data.append(t)
+
+print(sum(data))
+
+"""
+쉬운문제였다.
+스택에 대해 다시공부하게 해주는 좋은 문제다.
+"""

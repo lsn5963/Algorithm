7df211c9081bfebf2ea3b88d7ab9b50d441d0f09
이상남 <lsn5963@naver.com>
2022-09-17 14:19:22 +0900

    feat: 9184 신나는 함수 실행

---
+++ Backjoon/1904.py
@@ -0,0 +1,13 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+
+dp = [0]*(1000001)
+dp[1] = 1
+dp[2] = 2
+for i in range(3,n+1):
+    dp[i] = (dp[i-2] + dp[i-1]) % 15746
+print(dp[n])
+
+"""
+dp는 점화식을 구해야한다.
+"""

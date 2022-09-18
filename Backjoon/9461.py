d41b4967451565ed75f3737a112e7f693e14575e
이상남 <lsn5963@naver.com>
2022-09-18 17:48:41 +0900

    feat: 파도반 수열

---
+++ Backjoon/9461.py
@@ -0,0 +1,22 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+
+data = []
+dp = [0] * 101
+for _ in range(n):
+    data.append(int(sys.stdin.readline().rstrip()))
+
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+dp[4] = 2
+dp[5] = 2
+for i in range(6,101):
+    dp[i] = dp[i-5] + dp[i-1]
+for i in data:
+    print(dp[i])
+
+"""
+역시 점화식을 찾아 풀었다.
+다른 풀이를 보면 점화식이 다르지만 결과는 같게 나온다.
+"""

80f61526d3f0c7f1285f6ab7f818ef2f606ce9ee
lsn5963 <lsn5963@naver.com>
2022-09-19 14:51:35 +0900

    feat: 1912 연속합

--- Backjoon/1912.py
+++ Backjoon/1912.py
@@ -5,12 +5,11 @@ n = int(sys.stdin.readline().rstrip())
 p = list(map(int, sys.stdin.readline().rstrip().split()))
 
 
-dp = [0] *(n+1)
+dp = [0] *(n)
 dp[0] = p[0]
 for i in range(1, n):
     dp[i] = max(p[i], dp[i-1]+p[i])
 print(max(dp))
-
 """
 시간초과가 계속 나서 답지를 보고 풀었다.
 생각해내기는 어려웠지만 자꾸 접근하면 나아질 것 같다.

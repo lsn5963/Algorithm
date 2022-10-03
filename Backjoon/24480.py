eef3e8bbd9089faac34a10178f7a9b4074b79c26
이상남 <lsn5963@naver.com>
2022-10-03 14:32:07 +0900

    feat: 24480 알고리즘 수업 - 깊이 우선 탐색 2

---
+++ Backjoon/24480.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(10**6)
+cnt = 1
+def dfs(graph, v, visited):
+    global cnt
+    visited[v] = cnt
+
+    for i in graph[v]:
+        if visited[i] == 0:
+            cnt+=1
+            dfs(graph, i, visited)
+
+n,m,r = map(int, sys.stdin.readline().rstrip().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+for _ in range(m):
+    a,b = map(int, sys.stdin.readline().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1,n+1):
+    graph[i].sort(reverse=True)
+
+dfs(graph, r, visited)
+
+for i in visited[1:]:
+    print(i)
+
+"""
+rever = True를 왜 생각못했을까..
+"""

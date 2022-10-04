ba816696e94df3288c2d415e029e95781c47fda2
lsn5963 <lsn5963@naver.com>
2022-10-04 14:36:38 +0900

    feat: 24445 알고리즘 수업 - 너비 우선 탐색

---
+++ Backjoon/24445.py
@@ -0,0 +1,31 @@
+from itertools import count
+import sys
+from collections import deque
+n,m,r = map(int,sys.stdin.readline().rstrip().split())
+visited = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
+cnt = 1
+
+for _ in range(m):
+    a,b = map(int,sys.stdin.readline().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, n+1):
+    graph[i].sort(reverse=True)
+
+def bfs(r):
+    global cnt
+
+    q = deque([r])
+    visited[r] = 1
+    while(q):
+        v = q.popleft()
+        for i in graph[v]:
+            if visited[i] == 0:
+                cnt += 1
+                visited[i] = cnt
+                q.append(i)
+bfs(r)
+for i in visited[1:]:
+    print(i)

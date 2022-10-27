6bd235b32b00c6eaaf20091a6cd4af6250b76506
lsn5963 <lsn5963@naver.com>
2022-10-27 14:59:45 +0900

    refactor: 1753 최단경로

--- Backjoon/1753.py
+++ Backjoon/1753.py
@@ -1,49 +1,42 @@
+from cmath import inf
 import sys
 import heapq
 
-INF = float('inf')
-V, E = map(int, sys.stdin.readline().split())
-K = int(sys.stdin.readline())
-graph = [[] for _ in range(V + 1)]
-# 비용을 미리 무한대로 초기화
-answer = [INF] * (V + 1)
-queue = []
+v,e = map(int, sys.stdin.readline().rstrip().split())
+INF = int(1e9)
+k = int(sys.stdin.readline().rstrip())
+graph = [[] for _ in range(v+1)]
+distance = [INF] * (v + 1)
 
-# 경로 입력
-for i in range(E):
-    u, v, w = map(int, sys.stdin.readline().split())
-    graph[u].append([v, w])
+for _ in range(e):
+    u, b, w = map(int, sys.stdin.readline().rstrip().split())
+    graph[u].append((b,w))
 
 def dijkstra(start):
-    answer[start] = 0 # 자기 자신의 비용은 0이기 때문에 처음 부분은 0으로 초기화
-    heapq.heappush(queue, [0, start]) # 시작값을 0 으로 초기화(자기 자신이기 때문)
+    q = []
+    distance[start] = 0
+    heapq.heappush(q, (0, start))
 
-    while queue:
-        # 현재 노드, 현재 가중치를 pop한다.
-        current_w, current_node = heapq.heappop(queue)
+    while q:
+        dist, now = heapq.heappop(q)
 
-        # 최단 거리가 아닌경우 스킵
-        if answer[current_node] < current_w:
+        if distance[now] < dist:
             continue
 
-        for next_node, weight in graph[current_node]:
-            # 선택된 노드 거쳐서 인접 노드로 가는 가중치
-            next_w = current_w + weight
+        for i in graph[now]:
+            cost = dist + i[1]
 
-            # 기존의 최소 가중치 보다 더 작다면 교체한다.
-            # 그리고 다음 노드에 다음 가중치를 넣어준다.
-            if next_w < answer[next_node]:
-                answer[next_node] = next_w
-                heapq.heappush(queue, [next_w, next_node])
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-dijkstra(K)
+dijkstra(k)
 
-# 첫번째 부터 출력(0번째는 필요 없는값이기 때문에)
-for i in answer[1:]:
-    print(i if i != INF else "INF")
-
-
+for i in range(1, v+1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
 """
-이해가 되지 않아 그냥 코드를 복붙했다.
-다음에는 이걸 수정해서 다시 올려야겠다.
+다시공부했다. 어떻게 돌아가는지 이해하는게 쉽지않았다.
 """

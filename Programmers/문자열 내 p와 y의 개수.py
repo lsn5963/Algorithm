9e4b81911d18f4b4d6a7399e6a2657fd851f96dd
lsn5963 <lsn5963@naver.com>
2022-11-24 14:30:20 +0900

    .

--- Programmers/문자열 내 p와 y의 개수.py
+++ Programmers/문자열 내 p와 y의 개수.py
@@ -1,6 +1,21 @@
-def solution(s):
-    answer = True
-    s.u
-    
 
-    return True
+
+def solution(s):
+    s = s.lower()
+    p = 0
+    y = 0
+
+    for i in s:
+        if i == 'p':
+            p += 1
+        elif i == 'y':
+            y += 1
+
+    if p == y:
+        return True
+    else:
+        return False
+
+
+
+print(solution("Py"))

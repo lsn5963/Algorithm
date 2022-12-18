ab95fd09dc059834100f1a2bc9931cb8ad898aaf
이상남 <lsn5963@naver.com>
2022-12-18 13:57:44 +0900

    .

---
+++ Programmers/음양 더하기.py
@@ -0,0 +1,10 @@
+def solution(absolutes, signs):
+    sum = 0
+    for i in range(len(absolutes)):
+        if signs[i] == True:
+            sum += absolutes[i]
+        else:
+            sum += -absolutes[i]    
+    return sum
+
+print(solution([4,7,12],[True,False,True]))

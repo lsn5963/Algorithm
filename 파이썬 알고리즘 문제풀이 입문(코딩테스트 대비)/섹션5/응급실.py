41051c6168935b2c0d33a1040f92f14501b14287
이상남 <lsn5963@naver.com>
2023-02-19 01:57:13 +0900

    2023 02 19 01:57

---
+++ 파이썬 알고리즘 문제풀이 입문(코딩테스트 대비)/섹션5/응급실.py
@@ -0,0 +1,26 @@
+from collections import deque
+n,m = map(int, input().split())
+
+data = list(map(int, input().split()))
+data = deque(data)
+cnt = 0
+check = True
+data[m] = str(data[m])
+find = data[m]
+while data:
+    tmp = data.popleft()
+    for i in data:
+        if int(i) > int(tmp):
+            data.append(tmp)
+            break
+    else:
+        cnt += 1
+        if tmp == find:
+            check = False
+
+    if check == False:
+        break
+
+    
+        
+print(cnt)

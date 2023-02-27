4c855b44cfdbbb0885b51e91fb4022fdcd3f7fe0
lsn5963 <lsn5963@naver.com>
2023-02-27 17:14:16 +0900

    수 찾기

--- Backjoon/1920.py
+++ Backjoon/1920.py
@@ -1,31 +1,3 @@
-# n = int(input())
-# data = list(map(int, input().split()))
-
-# m = int(input())
-# ch = list(map(int, input().split()))
-
-# data.sort()
-
-# for i in ch:
-#     lt,rt = 0,len(data)-1
-#     check = False
-    
-#     while lt<=rt:
-#         mid = (lt+rt) // 2
-
-#         if data[mid] == i:
-#             print(1)
-#             check = True
-#             break
-#         if data[mid] > i:
-#             rt = mid - 1
-        
-#         if data[mid] < i:
-#             lt = mid + 1
-
-#     if check == False:
-#         print(0)
-
 n = int(input())
 data = list(map(int, input().split()))
 
@@ -34,16 +6,44 @@ ch = list(map(int, input().split()))
 
 data.sort()
 
-dic = {}
-
-for i in data:
-    if i not in dic:
-        dic[i] = 1
-    else:
-        dic[i] += 1
-
 for i in ch:
-    if i in dic:
-        print(dic[i])
-    else:
+    lt,rt = 0,len(data)-1
+    check = False
+    
+    while lt<=rt:
+        mid = (lt+rt) // 2
+
+        if data[mid] == i:
+            print(1)
+            check = True
+            break
+        if data[mid] > i:
+            rt = mid - 1
+        
+        if data[mid] < i:
+            lt = mid + 1
+
+    if check == False:
         print(0)
+
+# n = int(input())
+# data = list(map(int, input().split()))
+
+# m = int(input())
+# ch = list(map(int, input().split()))
+
+# data.sort()
+
+# dic = {}
+
+# for i in data:
+#     if i not in dic:
+#         dic[i] = 1
+#     else:
+#         dic[i] += 1
+
+# for i in ch:
+#     if i in dic:
+#         print(dic[i])
+#     else:
+#         print(0)

fbc0c17e11eb37a409dbc9a618716575f147c3a3
이상남 <lsn5963@naver.com>
2023-04-13 02:33:31 +0900

    .

--- UMC/해쉬/베스트앨범.py
+++ UMC/해쉬/베스트앨범.py
@@ -1,10 +1,25 @@
 def solution(genres, plays):
     dt = {}
-    for i in range(len(genres)):
+    csum = {}
+    for i,j in enumerate(genres):
         if genres[i] in dt:
-            dt[genres[i]] = plays[i]
+            dt[genres[i]].append([plays[i],i])
         else:
-            dt[genres[i]] += plays[i]
-    # print(1)
-    # return dt.keys()
-print(solution(["classic", "pop", "classic", "classic", "pop"]	,[500, 600, 150, 800, 2500]))
+            dt[genres[i]] = [[plays[i],i]]
+    for i in range(len(genres)):
+        if genres[i] in csum:
+            csum[genres[i]] += plays[i]
+        else:
+            csum[genres[i]] = plays[i]
+    csum = sorted(csum.items(), key = lambda x: x[1], reverse = True)
+    result = []
+    for i in csum:
+        dt[i[0]] = sorted(dt[i[0]], key = lambda x:x[0] , reverse = True)
+        for j in range(2):
+            if len(dt[i[0]]) == 1:
+                result.append(dt[i[0]][0][1])
+                break
+            result.append(dt[i[0]][j][1])
+    return result
+print(solution(["classic", "classic", "classic", "classic", "pop"]	,[500, 150, 800, 800, 2500]))
+

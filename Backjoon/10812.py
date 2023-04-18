d81e7d8561ea00e58174130973dec11c9ca803b0
이상남 <lsn5963@naver.com>
2023-04-18 23:39:06 +0900

    바구니 순서 바꾸기

--- Backjoon/10812.py
+++ Backjoon/10812.py
@@ -1,6 +1,8 @@
-n = input()
+n, m = map(int,input().split())
+data = [list(map(int,input().split())) for _ in range(m)]
+rst = list(range(1,n+1))
+for i,j,k in data:
+    rst = rst[:i-1]+rst[k-1:j] + rst[i-1:k-1] + rst[j:]
 
-if n == n[::-1]:
-    print(1)
-else:
-    print(0)
+for i in rst:
+    print(i, end = " ")

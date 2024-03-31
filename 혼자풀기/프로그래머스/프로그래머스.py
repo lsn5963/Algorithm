d0ff3fa9baf2ff6cc332e8ebadd4b2c8deedcaf0
lsn5963 <lsn5963@naver.com>
2024-03-31 20:15:41 +0900

    2024 03 31

---
+++ 혼자풀기/프로그래머스/프로그래머스.py
@@ -0,0 +1,16 @@
+def solution(s):
+    s = s.split(" ")
+    rst = []
+    # print(s)
+    for i in s:
+        tmp = []
+        for j in range(len(i)):
+            if j == 0:
+                tmp.append(i[0].upper())
+            else:
+                tmp.append(i[j].lower())
+        rst.append("".join(tmp))
+    return " ".join(rst)
+
+
+

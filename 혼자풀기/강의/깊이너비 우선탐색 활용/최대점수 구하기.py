32f8192c2ce545f4a882ffeda089ccbf032f6024
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2024-07-31 23:49:10 +0900

    2024 07 31

--- 혼자풀기/강의/깊이너비 우선탐색 활용/최대점수 구하기.py
+++ 혼자풀기/강의/깊이너비 우선탐색 활용/최대점수 구하기.py
@@ -1,19 +1,17 @@
-n,m = map(int, input().split())
-
-data = [list(map(int, input().split())) for _ in range(n)]
-print(data)
-ans = 0
-sum = 0
-time = 0
-def dfs(v,sum,time):
-    global ans
-    if time > m:
+n,m = map(int,input().split())
+data = [list(map(int,input().split())) for _ in range(n)]
+rst = 0
+tot = sum(a for a,b in data)
+def dfs(num,s,t,tsum):
+    global rst
+    if tot - tsum + s < rst:
         return
-    if v == n:
-        ans = max(ans,sum)
+    if t > m:
         return
-    dfs(v+1,sum+data[v][0],time+data[v][1])
-    dfs(v+1,sum,time)
-
-dfs(0,sum,time)
-print(ans)
+    if n == num:
+        rst = max(rst,s)
+    else:
+        dfs(num + 1,s + data[num][0],t+data[num][1],tsum+data[num][0])
+        dfs(num + 1, s, t, tsum + data[num][0])
+dfs(0,0,0,0)
+print(rst)

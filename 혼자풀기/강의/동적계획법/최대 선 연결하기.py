926fa9c10ee5c6727020742547c4bcd092cc005f
lsn5963 <lsn5963@naver.com>
2024-08-20 18:48:38 +0900

    2024 08 20

---
+++ 혼자풀기/강의/동적계획법/최대 선 연결하기.py
@@ -0,0 +1,17 @@
+n = int(input())
+data = list(map(int, input().split()))
+dp = [0]*n
+dp[0] = 1
+m = 0
+for i in range(1,n):
+    m = 0
+    for j in range(i-1,-1,-1):
+        if data[j] < data[i]:
+            if dp[j] > m:
+                m = dp[j]
+    dp[i] = m + 1
+
+
+
+
+print(max(dp))
